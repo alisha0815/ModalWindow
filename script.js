da6d3cfc828248f3cf878262b823eaf9a219af95
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-11-08 13:38:43 +0100

    adding and removing modal

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+const btnsCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  //   console.log(btnsOpenModal[i].textContent);
+  //   open button
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// Close button
+btnsCloseModal.addEventListener('click', closeModal); // button
+overlay.addEventListener('click', closeModal); // overlay
